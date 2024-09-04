8a5ff618181ec59a040be5a9bbadcd5f1e8207f8
PrashannaR <prashanna.rajbhandari1@gmail.com>
2024-09-04 07:20:34 +0530

    feat: initial commit

---
+++ Jhilke/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jhilke
+//
+//  Created by Prashanna Rajbhandari on 04/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to Jhilke")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jhilke/JhilkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JhilkeApp.swift
+//  Jhilke
+//
+//  Created by Prashanna Rajbhandari on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct JhilkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
