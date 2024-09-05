73f99d11ef543b2b9bba7bdcb3840c80700d8426
PrashannaR <prashanna.rajbhandari1@gmail.com>
2024-09-06 01:28:17 +0530

    feat: added core display framework and waydabber's apple silicon ddc impl

---
+++ Jhilke/Utils/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  Jhilke
+//
+//  Created by Prashanna Rajbhandari on 06/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Bridging_Header : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Jhilke/Utils/Bridging-Header.m
@@ -0,0 +1,12 @@
+//
+//  Bridging-Header.m
+//  Jhilke
+//
+//  Created by Prashanna Rajbhandari on 06/09/2024.
+//
+
+#import "Bridging-Header.h"
+
+@implementation Bridging_Header
+
+@end
