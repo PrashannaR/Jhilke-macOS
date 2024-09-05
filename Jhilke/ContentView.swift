72ac7704c8b968bac4d34b1ca9336c78a43233a7
PrashannaR <prashanna.rajbhandari1@gmail.com>
2024-09-05 07:57:41 +0530

    feat: file and design cleanup

--- Jhilke/ContentView.swift
+++ Jhilke/ContentView.swift
@@ -11,10 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Welcome to Jhilke")
+            MonitorBrightnessControl()
         }
         .padding()
     }
--- Jhilke/MonitorBrightnessControl.swift
+++ Jhilke/MonitorBrightnessControl.swift
@@ -5,19 +5,31 @@
 //  Created by Prashanna Rajbhandari on 04/09/2024.
 //
 
+
 import SwiftUI
-import DDC
 
 struct MonitorBrightnessControl: View {
     @State private var selectedScreenIndex = 0
     let screens = NSScreen.screens
-    @State private var ddc: DDC?
-    
+    @State private var brightness: Float = 50.0
+
     var body: some View {
-        VStack(alignment: .leading){
-            ForEach(0..<screens.count, id: \.self){ screen in
-                Text("\(screens[screen].localizedName)")
+        VStack(alignment: .leading) {
+            Text("Control Screen Brightness")
+                .font(.title2)
+
+            Picker("Select Screen", selection: $selectedScreenIndex) {
+                ForEach(0 ..< screens.count, id: \.self) { index in
+                    Text("Screen \(index + 1): \(screens[index].localizedName)")
+                }
             }
+            .pickerStyle(.radioGroup)
+
+            Slider(value: $brightness, in: 0 ... 100, step: 1) {
+                Text("Brightness")
+            }
+            .frame(width: 300)
+            
         }
         .padding()
     }
@@ -26,3 +38,4 @@ struct MonitorBrightnessControl: View {
 #Preview {
     MonitorBrightnessControl()
 }
+
